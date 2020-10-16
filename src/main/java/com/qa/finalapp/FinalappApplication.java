a5d9318cffd7153aee6d709a6eda06c0bd26597f
Paulo Silva <Psilva@qa.com>
2020-10-16 13:37:23 +0100

    added spring boot app files

---
+++ src/main/java/com/qa/finalapp/FinalappApplication.java
@@ -0,0 +1,13 @@
+package com.qa.finalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalappApplication.class, args);
+    }
+
+}
