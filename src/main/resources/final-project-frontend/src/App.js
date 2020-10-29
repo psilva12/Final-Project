f159547a6de677154be50442d0d7dd9be7a58d17
Jordan Hamilton <jordanhamilton31@hotmail.com>
2020-10-29 14:38:27 +0000

    Added functionality to update and delete ticket requests through frontend

--- src/main/resources/final-project-frontend/src/App.js
+++ src/main/resources/final-project-frontend/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
+import DeleteTicket from './components/DeleteTicket';
 import GetTicket from './components/GetTicket';
 import PostTicket from './components/PostTicket';
+import UpdateTicket from './components/UpdateTicket';
 
 function App() {
   return (
       <>
         <GetTicket />
         <PostTicket />
+        <DeleteTicket />
+        <UpdateTicket />
       </>
   );
 }
---
+++ src/main/resources/final-project-frontend/src/components/UpdateTicket.jsx
@@ -0,0 +1,62 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+export default class UpdateTicket extends React.Component {
+
+    state = {
+        id: '',
+        title: '',
+        description: ''
+      }
+    
+      handleChange = event => {
+        this.setState({ id: event.target.value});
+      }
+
+      handleTitleChange = event => {
+          this.setState({title: event.target.value})
+      }
+    
+      handleDescChange = event => {
+        this.setState({description: event.target.value})
+      }
+
+      handleSubmit = event => {
+        event.preventDefault();
+        
+        axios.put(`http://localhost:9500/updateTicket/${this.state.id}`,
+            {   title: this.state.title,
+                description: this.state.description
+            }
+        )
+          .then(res => {
+            console.log(res);
+            console.log(res.data);
+          })
+
+      }
+
+    render() {
+      return (
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Ticket ID:
+              <input type="text" name="id" onChange={this.handleChange} />
+              <br>
+              </br>
+              Title:
+              <input type="text" name="title" onChange={this.handleTitleChange} />
+              <br>
+              </br>
+              Description:
+              <input type="text" name="description" onChange={this.handleDescChange} />
+              <br>
+              </br>
+            </label>
+            <button type="submit">Update</button>
+          </form>
+        </div>
+      )
+    }
+}
