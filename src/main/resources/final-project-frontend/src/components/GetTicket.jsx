679c1f8995f622583f27382df3f1895709f79f63
Judith E <judith.edhogbo@gmail.com>
2020-11-03 11:37:29 +0000

    Sorted by done and to do

--- src/main/resources/final-project-frontend/src/components/GetTicket.jsx
+++ src/main/resources/final-project-frontend/src/components/GetTicket.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import Card from 'react-bootstrap/Card'
 import TimeAgo from 'react-timeago'
 import ViewTicket from './ViewTicket'
+import CardColumns from 'react-bootstrap/CardColumns'
 
 
 
@@ -41,8 +42,13 @@ const GetTicket = () => {
     }else{
         return(
             <div>
-                {items.map( (hi) => (
+                 <p> Queue </p>
+                 <CardColumns>
+                     
+                {items.filter(ticket =>ticket.status === false).map( (hi) => (
                     <div>
+                       
+                      
                         <Card style={{ width: '18rem' }}>
                             <Card.Body>
                             <Card.Title>{hi.title}</Card.Title>
@@ -63,7 +69,37 @@ const GetTicket = () => {
                         </Card> 
                         <br></br>
                     </div>
+
                 ))} 
+                </CardColumns> 
+
+                <p> Done </p>
+                <CardColumns>
+                {items.filter(ticket =>ticket.status === true).map( (hi) => (
+                    <div>
+                        
+                      
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Body>
+                            <Card.Title>{hi.title}</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">{hi.author}</Card.Subtitle>
+                            <Card.Text>
+                                {hi.description}
+                            </Card.Text>
+                            <Card.Text>
+                            <TimeAgo date={hi.time}>{({ value }) => <h2>{value}</h2>}</TimeAgo>
+                            </Card.Text>
+                            <button>
+                            <a href={`/viewTicket/${hi.id}`}> View Ticket</a>
+                            </button>
+                            </Card.Body> 
+                        </Card> 
+                        <br></br>
+                    </div>
+
+                ))} 
+                </CardColumns> 
+
             </div>
         )
     }
