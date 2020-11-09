89dcb038caf8c9935396de3a5676e8c4b728a494
JudithEdh <68279772+JudithEdh@users.noreply.github.com>
2020-11-09 10:29:53 +0000

    Add files via upload

---
+++ src/test/java/com/qa/finalapp/rest/TicketIntegrationTests.java
@@ -0,0 +1,97 @@
+package com.qa.finalapp.rest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.qa.finalapp.domain.Comment;
+import com.qa.finalapp.domain.Ticket;
+import com.qa.finalapp.dto.TicketDTO;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultMatcher;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@Sql(scripts = {"classpath:ticket-schema.sql", "classpath:ticket-data.sql" }, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+//@Sql(scripts = {"classpath:ticket-data.sql" }, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+public class TicketIntegrationTests {
+
+    @Autowired
+    private MockMvc mockMVC;
+
+    @Autowired
+    private ObjectMapper mapper;
+    @Test
+    void testCreate() throws Exception {
+        List<Comment> comments = new ArrayList<>();
+        Ticket newTicket= new Ticket("a", comments, "b", "c", "d", false);
+        String requestBody = this.mapper.writeValueAsString(newTicket);
+        RequestBuilder request = post("/createTicket").contentType(MediaType.APPLICATION_JSON).content(requestBody);
+
+        ResultMatcher checkStatus = status().is(201);
+
+        TicketDTO savedTicket= new TicketDTO("a", "b", "d", "c", false);
+        savedTicket.setId(2L);
+
+        MvcResult result = this.mockMVC.perform(request).andExpect(checkStatus).andReturn();
+
+        String reqBody = result.getResponse().getContentAsString();
+
+        TicketDTO ticketResult = this.mapper.readValue(reqBody, TicketDTO.class);
+        Assertions.assertThat(ticketResult).isEqualToIgnoringGivenFields(savedTicket);
+    }
+
+    @Test
+    void testUpdate() throws Exception {
+        List<Comment> comments = new ArrayList<>();
+        TicketDTO newTicket= new TicketDTO("a", "b", "c", "d", false);
+        String requestBody = this.mapper.writeValueAsString(newTicket);
+
+        RequestBuilder request = put("/updateTicket/1").contentType(MediaType.APPLICATION_JSON).content(requestBody);
+
+
+        ResultMatcher checkStatus = status().isOk();
+        TicketDTO savedTicket= new TicketDTO("a", "b", "c", "d", false);
+        savedTicket.setId(1L);
+
+        MvcResult result = this.mockMVC.perform(request).andExpect(checkStatus).andReturn();
+
+        String reqBody = result.getResponse().getContentAsString();
+
+        TicketDTO ticketResult = this.mapper.readValue(reqBody, TicketDTO.class);
+        Assertions.assertThat(ticketResult).isEqualToIgnoringGivenFields(savedTicket);
+    }
+    @Test
+    void testDelete() throws Exception {
+        RequestBuilder request = delete("/deleteTicket/1");
+
+        ResultMatcher checkStatus = status().is(204);
+
+        this.mockMVC.perform(request).andExpect(checkStatus);
+
+    }
+
+    @Test
+    void testRead() throws Exception {
+        List<Comment> comments = new ArrayList<>();
+        TicketDTO ticket= new TicketDTO("a", "b", "c", "d", false);
+        ticket.setId(1L); // badger object to match the one in badger-data.sql
+        List<TicketDTO> tickets = new ArrayList<>();
+        tickets.add(ticket);
+        String responseBody = this.mapper.writeValueAsString(tickets);
+
+        this.mockMVC.perform(get("/getTickets")).andExpect(status().isOk()).andExpect(content().json(responseBody));
+    }
+}
---
+++ src/test/java/com/qa/finalapp/service/TicketServiceUnitTest.java
@@ -0,0 +1,113 @@
+package com.qa.finalapp.service;
+import com.qa.finalapp.domain.Comment;
+import com.qa.finalapp.domain.Ticket;
+import com.qa.finalapp.dto.CommentDTO;
+import com.qa.finalapp.dto.TicketDTO;
+import com.qa.finalapp.repo.TicketRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@ActiveProfiles(profiles = "test")
+public class TicketServiceUnitTest {
+    @Autowired
+    private TicketService service;
+
+    @MockBean
+    private TicketRepository repo;
+
+    @MockBean
+    private ModelMapper mapper;
+    TicketDTO mapToDTO(Ticket ticket){
+        return this.mapper.map(ticket, TicketDTO.class);
+    }
+    @Test
+    void testCreate() {
+        Long id = 1L;
+        List<Comment> comments = new ArrayList<>();
+        Ticket newTicket = new Ticket("a", comments, "b", "c", "d", false);
+        Ticket savedTicket = new Ticket("a", comments, "b", "c", "d", false);
+
+        savedTicket.setId(id);
+        Mockito.when(this.repo.save(newTicket)).thenReturn(savedTicket);
+
+        assertThat(this.service.createTicket(newTicket)).isEqualTo(mapToDTO(savedTicket));
+
+        Mockito.verify(this.repo, Mockito.times(1)).save(newTicket);
+    }
+
+    @Test
+    void testUpdate() {
+        Long id = 1L;
+        List<Comment> comments = new ArrayList<>();
+        Ticket newTicket = new Ticket("a", comments, "b", "c", "d", false);
+        Ticket oldTicket = new Ticket("a", comments, "b", "c", "d", false);
+        oldTicket.setId(id);
+
+        Ticket updatedTicket = new Ticket("a", comments, "b", "c", "d", false);
+        updatedTicket.setId(id);
+        Mockito.when(this.repo.findById(id)).thenReturn(Optional.of(oldTicket));
+
+        Mockito.when(this.repo.save(updatedTicket)).thenReturn(updatedTicket);
+
+        assertThat(this.service.updateTicket(id, newTicket)).isEqualTo(mapToDTO(this.repo.save(updatedTicket)));
+
+
+        Mockito.verify(this.repo, Mockito.times(1)).findById(id);
+        Mockito.verify(this.repo, Mockito.times(1)).save(updatedTicket);
+    }
+
+    @Test
+    void testGet() {
+
+        List<Comment> comments = new ArrayList<>();
+        Ticket ticket = new Ticket("a", comments, "b", "c", "d", false);
+        ticket.setId(1L); // badger object to match the one in badger-data.sql
+        List<Ticket> tickets = new ArrayList<>();
+        tickets.add(ticket);
+
+        Mockito.when(this.repo.findAll()).thenReturn(tickets);
+
+        // THEN
+        assertThat(this.service.readAllTickets()).isEqualTo(tickets.stream().map(this::mapToDTO).collect(Collectors.toList()));
+
+        Mockito.verify(this.repo, Mockito.times(1)).findAll();
+    }
+
+    @Test
+    void testDelete() {
+        // GIVEN
+        Long id = 1L;
+        boolean found = false;
+
+        // WHEN
+        Mockito.when(this.repo.existsById(id)).thenReturn(!found);
+
+        // THEN
+        assertThat(this.service.deleteTicket(id)).isEqualTo(!found);
+
+        Mockito.verify(this.repo, Mockito.times(2)).existsById(id);
+    }
+}
+
+
+
+
+
